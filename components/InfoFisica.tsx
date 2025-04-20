68cea7557a77a5c7f7ebecfcb8607bd70b8baa86
Joel Lawi <abecedegluu@gmail.com>
2025-04-19 20:30:19 -0500

    Ajustes style

--- components/InfoFisica.tsx
+++ components/InfoFisica.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function InfoFisica(props) {
   const { title, description } = props
   return (
-    <View style={styles.contanier}>
+    <View /*style={styles.container}*/ >
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.description}>{description}</Text>
     </View>
@@ -12,14 +12,14 @@ export default function InfoFisica(props) {
 
 const styles = StyleSheet.create({
 
-  contanier: {
-
-  },
+  /*container: {
+    justifyContent: 'center', alignItems: 'center', padding: 30
+  },*/
   title: {
-
+    fontSize: 26, fontWeight: 'bold', color: 'white', textAlign: 'center', marginBottom: 15, width: 200
   },
   description: {
-
+    fontSize: 18, color: 'white', opacity: 0.6, textAlign: 'center', width: 200
   }
 
 })
