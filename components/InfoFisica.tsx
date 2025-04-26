71c1a86fe13d4af73e461353df587d9225603896
Joel Lawi <abecedegluu@gmail.com>
2025-04-25 22:18:07 -0500

    Añadiendo props de img

--- components/InfoFisica.tsx
+++ components/InfoFisica.tsx
@@ -1,11 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function InfoFisica(props) {
-  const { title, description } = props
+
+  const { title, description, img } = props
   return (
     <View /*style={styles.container}*/ >
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
+      <Image source={img} style={styles.img} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
     </View>
   )
 }
@@ -16,10 +20,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center', alignItems: 'center', padding: 30
   },*/
   title: {
-    fontSize: 26, fontWeight: 'bold', color: 'white', textAlign: 'center', marginBottom: 15, width: 200
+    fontSize: 25, fontWeight: 'bold', color: 'white', textAlign: 'center', marginBottom: 15, width: 300
   },
   description: {
-    fontSize: 18, color: 'white', opacity: 0.6, textAlign: 'center', width: 200
+    fontSize: 18, color: 'white', opacity: 0.6, textAlign: 'center', width: 300
+  },
+  img: {
+    width: 240, height: 240, marginBottom: 20, marginLeft: 30
+
+  },
+  textContainer: {
+    marginTop: 10
   }
 
 })
