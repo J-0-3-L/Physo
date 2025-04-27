e876ca070065d5b9fa0bc7af7491d8371ea883b8
Joel Lawi <abecedegluu@gmail.com>
2025-04-26 22:35:04 -0500

    Integrando font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
 import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Text as DefaultText } from "react-native";
 
 export default function RootLayout() {
+
+  const [fontsLoaded] = useFonts({
+    'Onest-Regular': require("../assets/fonts/Onest-Regular.ttf"),
+  });
+
   return (
     <>
       <Stack>
